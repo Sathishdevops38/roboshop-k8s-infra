401382a86318c8e638ff127e88f022f0e152199e
sathishdevops38 <sathishdevops38@gmail.com>
2025-11-30 17:08:32 +0530

    fix script

--- 00-terraform-k8s-ec2/k8s.sh
+++ 00-terraform-k8s-ec2/k8s.sh
@@ -10,3 +10,14 @@ sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin dock
 sudo systemctl start docker
 sudo systemctl enable docker
 sudo usermod -aG docker ec2-user
+
+#install kubectl
+ curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
+ sudo install -o root -g root -m 0755 kubectl /usr/local/bin/kubectl
+
+ #install eksctl
+ ARCH=amd64
+ PLATFORM=$(uname -s)_$ARCH
+ curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+ tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+ sudo mv /tmp/eksctl /usr/local/bin
