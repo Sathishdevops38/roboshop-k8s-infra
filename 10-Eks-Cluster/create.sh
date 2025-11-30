f16494b984b4c29a2cd852147d4c13f37853e880
sathishdevops38 <sathishdevops38@gmail.com>
2025-11-30 17:19:18 +0530

    create

--- 10-Eks-Cluster/create.sh
+++ 10-Eks-Cluster/create.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #eksctl create cluster --name demo-cluster --region us-west-2 --node-type t3.micro --nodes 2
-eksctl create cluster -f ./create-eks.yaml --wait
+eksctl create cluster -f ./create-eks.yaml 
 if [ $? -eq 0 ]; then
     echo "EKS cluster created successfully."
     aws eks update-kubeconfig --region us-west-2 --name roboshop-eks-cluster
