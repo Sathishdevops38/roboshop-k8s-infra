df51dffc8ca2373373f67d0ea3d4fd119f566c24
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-01 11:18:30 +0530

    deletion

--- 10-Eks-Cluster/delete.sh
+++ 10-Eks-Cluster/delete.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+aws cloudformation update-termination-protection \
+    --stack-name eksctl-roboshop-eks-cluster-nodegroup-roboshop-dev-ng \
+    --no-enable-termination-protection
+aws cloudformation update-termination-protection \
+    --stack-name eksctl-roboshop-eks-cluster-cluster \
+    --no-enable-termination-protection    
 eksctl delete nodegroup --cluster=roboshop-eks-cluster --all
 eksctl delete cluster --name=roboshop-eks-cluster --wait
