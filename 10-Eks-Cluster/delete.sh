2074b7ff1fdb33bcf2206159824937397a556368
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-08 11:43:21 +0530

    rm all

--- 10-Eks-Cluster/delete.sh
+++ 10-Eks-Cluster/delete.sh
@@ -5,5 +5,5 @@ aws cloudformation update-termination-protection \
 aws cloudformation update-termination-protection \
     --stack-name eksctl-roboshop-eks-cluster-cluster \
     --no-enable-termination-protection    
-eksctl delete nodegroup --cluster=roboshop-eks-cluster --all
+eksctl delete nodegroup --cluster=roboshop-eks-cluster 
 eksctl delete cluster --name=roboshop-eks-cluster --wait
