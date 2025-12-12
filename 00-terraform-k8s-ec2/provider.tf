20eb434ca81854810ba8338fbad65d0bb562dc39
sathishdevops38 <sathishdevops38@gmail.com>
2025-12-12 12:17:37 +0530

    ami update

--- 00-terraform-k8s-ec2/provider.tf
+++ 00-terraform-k8s-ec2/provider.tf
@@ -6,7 +6,7 @@ terraform {
     }
   }
   backend "s3" {
-    bucket = "terraform-backend-86s"
+    bucket = "terraform-backend-86"
     key    = "roboshop-k8s-ec2"
     region = "us-west-2"
     use_lockfile = true
